24714ec2d08ab354a92bd530f38e35d7e7a7bc5e
Symphony <68447349+symphonymarie@users.noreply.github.com>
2024-09-11 16:04:10 -0700

    Bug clean up

--- background.js
+++ background.js
@@ -1,14 +1,46 @@
-let Engine1 = "https://www.duckduckgo.com/";
+let Engine1 = "duckduckgo.com/";
 let Engine2 = "https://www.google.com/";
+
+/*
+Initialize the page action: set icon and title, then show.
+Only operates on tabs using the engines listed above.
+*/
+
+function initializePageAction(tab) {
+  browser.pageAction.onClicked.addListener(onClick);
+  if (tab.url.includes(Engine1)) {
+    browser.pageAction.show(tab.id);
+    newurl = tab.url + 'search options&df=1989-01-01..2021-12-31';
+  }
+  if (tab.url.includes(Engine2)) {
+    browser.pageAction.show(tab.id);
+    newurl = tab.url + '&tbs=cdr%3A1%2Ccd_min%3A1989%2Ccd_max%3A2021&tbm=';
+  }
+}
+
+var gettingAllTabs = browser.tabs.query({});
+gettingAllTabs.then((tabs) => {
+  for (let tab of tabs) {
+    initializePageAction(tab);
+  }
+});
+
+browser.tabs.onUpdated.addListener((id, changeInfo, tab) => {
+  initializePageAction(tab);
+});
 
 /*
 On page action click, filter search results.
 */
-browser.pageAction.onClicked.addListener(() => {
-  if (tab.url = string.includes(Engine1, 0)) {
-    window.location = location.href + 'search options&df=1989-01-01..2021-12-31';
+
+browser.pageAction.onClicked.addListener(onClick);
+function onClick() {
+
+browser.pageAction.onClicked.addListener(Unshit);
+
+function Unshit (){
+    browser.tabs.create({
+      "url": newurl,
+    });
   }
-  if (tab.url = string.includes(Engine2, 0)) {
-    window.location = location.href + '&tbs=cdr%3A1%2Ccd_min%3A1989%2Ccd_max%3A2021&tbm=';
-  }
-});
+}
