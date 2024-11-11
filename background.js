48aa2702ad9d193d21bc88aab846683091ea5808
Symphony <68447349+symphonymarie@users.noreply.github.com>
2024-11-10 21:15:00 -0800

    Update background.js
    Fixed error that sometimes opened multiple copies of the new tab

--- background.js
+++ background.js
@@ -1,5 +1,7 @@
 let Engine1 = "duckduckgo.com/";
 let Engine2 = "https://www.google.com/";
+let String1 = "1989-01-01"
+let String2 = "%3A1989%"
 
 /*
 Initialize the page action: set icon and title, then show.
@@ -9,11 +11,21 @@ Only operates on tabs using the engines listed above.
 function initializePageAction(tab) {
   browser.pageAction.onClicked.addListener(onClick);
   if (tab.url.includes(Engine1)) {
-    browser.pageAction.show(tab.id);
+    if (tab.url.includes(String1)) {
+    browser.pageAction.hide(tab.id);
+    }
+    else {
+      browser.pageAction.show(tab.id);
+      }
     newurl = tab.url + 'search options&df=1989-01-01..2021-12-31';
   }
   if (tab.url.includes(Engine2)) {
-    browser.pageAction.show(tab.id);
+      if (tab.url.includes(String2)) {
+        browser.pageAction.hide(tab.id);
+      }
+      else {
+        browser.pageAction.show(tab.id);
+        }
     newurl = tab.url + '&tbs=cdr%3A1%2Ccd_min%3A1989%2Ccd_max%3A2021&tbm=';
   }
 }
