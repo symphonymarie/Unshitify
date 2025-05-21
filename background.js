3312c230a8983aad1afcb20b442404608a1a6e63
Symphony <68447349+symphonymarie@users.noreply.github.com>
2025-05-21 00:04:20 -0700

    Update background.js
    Fixed having to click the icon twice to initialize the add-on when using it for the first time in a session.
    Released on Android - Go to Extensions and click the icon to update a search.

--- background.js
+++ background.js
@@ -1,6 +1,7 @@
 let Engine1 = "duckduckgo.com/";
 let Engine2 = "https://www.google.com/";
-let String1 = "1989-01-01"
+let StartDate = "1989-01-01"
+let EndDate = "2021-12-31"
 let String2 = "%3A1989%"
 
 /*
@@ -9,25 +10,25 @@ Only operates on tabs using the engines listed above.
 */
 
 function initializePageAction(tab) {
-  browser.pageAction.onClicked.addListener(onClick);
+  // browser.pageAction.onClicked.addListener(onClick);
   if (tab.url.includes(Engine1)) {
-    if (tab.url.includes(String1)) {
+    if (tab.url.includes(StartDate)) {
     browser.pageAction.hide(tab.id);
     }
     else {
       browser.pageAction.show(tab.id);
       }
-    newurl = tab.url + 'search options&df=1989-01-01..2021-12-31';
+      newurl = tab.url + '&search options&df=' + (StartDate) + '..' + (EndDate);
   }
   if (tab.url.includes(Engine2)) {
-      if (tab.url.includes(String2)) {
-        browser.pageAction.hide(tab.id);
+    if (tab.url.includes(String2)) {
+      browser.pageAction.hide(tab.id);
+    }
+    else {
+      browser.pageAction.show(tab.id);
       }
-      else {
-        browser.pageAction.show(tab.id);
-        }
-    newurl = tab.url + '&tbs=cdr%3A1%2Ccd_min%3A1989%2Ccd_max%3A2021&tbm=';
-  }
+  newurl = tab.url + '&tbs=cdr%3A1%2Ccd_min%3A1989%2Ccd_max%3A2021&tbm=';
+}
 }
 
 var gettingAllTabs = browser.tabs.query({});
@@ -47,7 +48,7 @@ On page action click, filter search results.
 
 browser.pageAction.onClicked.addListener(onClick);
 function onClick() {
-
+Unshit();
 browser.pageAction.onClicked.addListener(Unshit);
 
 function Unshit (){
@@ -56,3 +57,8 @@ function Unshit (){
     });
   }
 }
+
+// function Revert() {
+//   EndDate = '2021/31/12';
+//   StartDate = '1989/01/01';
+// }
